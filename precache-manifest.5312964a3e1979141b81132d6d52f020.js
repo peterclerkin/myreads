e7fbd9185fc53dec2cac7b72244d814a6b9265db
Peter <clerkin07@gmail.com>
2019-04-18 15:34:33 +0100

    Updates

--- precache-manifest.5312964a3e1979141b81132d6d52f020.js
+++ precache-manifest.5312964a3e1979141b81132d6d52f020.js
@@ -1,34 +1,34 @@
 self.__precacheManifest = [
   {
-    "revision": "9d76939f674f5344d6e9",
+    "revision": "a0516a440564b9cfe2c2",
     "url": "/myreads/static/css/main.eb77be72.chunk.css"
   },
   {
-    "revision": "9d76939f674f5344d6e9",
-    "url": "/myreads/static/js/main.222a25e1.chunk.js"
+    "revision": "a0516a440564b9cfe2c2",
+    "url": "/myreads/static/js/main.243803cd.chunk.js"
   },
   {
     "revision": "ae7976efa8cc24264f0e",
     "url": "/myreads/static/js/runtime~main.7df25c17.js"
   },
   {
-    "revision": "66012778773925fb044a",
-    "url": "/myreads/static/js/2.f42bbf7b.chunk.js"
+    "revision": "de92beb15ea7e95e0186",
+    "url": "/myreads/static/js/2.63c665a2.chunk.js"
   },
   {
     "revision": "bb9363b86b02777d23523851826414e0",
     "url": "/myreads/static/media/add.bb9363b8.svg"
   },
   {
-    "revision": "3687aa988bec28f17ae39153db1646c3",
-    "url": "/myreads/static/media/arrow-back.3687aa98.svg"
-  },
-  {
     "revision": "78c6f53b2342643639fe1cbaf171bf47",
     "url": "/myreads/static/media/arrow-drop-down.78c6f53b.svg"
   },
   {
-    "revision": "c81c55abcb0336e5920f316bc2158a41",
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/myreads/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "b41d121dc489683f7dccde88172be629",
     "url": "/myreads/index.html"
   }
 ];
